c05d50ef607f01611bb2d0dd9b47616fe1a376f2
Unknown <kamil.dmytrenko@gmail.com>
2018-03-21 22:26:42 +0100

    chat app

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    const socket = io();
+    $('form').submit(() => {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', (msg) => {
+        $('#messages').append($('<li>').text(msg));
+    });
+});
